6635d17ae78163963ea9c16842e9201a08bd92f4
vitali kozhar <vox@mail.com>
2025-03-10 17:13:59 +0400

    solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -199,9 +199,14 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  return this;
 }
+Rectangle.prototype.getArea = function getArea() {
+  return this.width * this.height;
+};
 
 /**
  * Returns the JSON representation of specified object
