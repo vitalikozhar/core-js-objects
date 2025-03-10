fad8fea94287f743f5a3d287e39adeff74fd9af7
vitali kozhar <vox@mail.com>
2025-03-10 17:20:46 +0400

    solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -218,8 +218,8 @@ Rectangle.prototype.getArea = function getArea() {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -233,8 +233,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 /**
